c1522ebe57c4189bd1d4b5b55cdd0590536c5161
Jonathan-PSU <jonatr35@pdx.edu>
2022-07-07 08:31:32 -0700

    Almost working, can't connect to ftp

---
+++ src/main/java/com/example/FtpClient.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class FtpClient {
+
+    private String server;
+    private int port;
+    private String user;
+    private String password;
+    private FTPClient ftp;
+
+    // constructor
+    FtpClient(String server, int port, String user, String password) {
+        this.server = server;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+    }
+
+    
+    void open() throws IOException {
+        System.out.println("Connecting...");
+        ftp = new FTPClient();
+
+        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+
+        ftp.connect(server, port);
+        int reply = ftp.getReplyCode();
+        System.out.println("Reply : " + reply);
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new IOException("Exception in connecting to FTP Server");
+        }
+
+        ftp.login(user, password);
+    }
+
+    void close() throws IOException {
+        System.out.println("Server closing");
+        ftp.disconnect();
+    }
+}
